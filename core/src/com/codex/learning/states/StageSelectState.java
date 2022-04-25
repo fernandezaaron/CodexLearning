c06b2401d3f61fb853fa93478a038b844ba4b97b
Paul Co <79306634+Paul-26-Co@users.noreply.github.com>
2022-04-25 13:19:06 +0800

    StageSelectState Coordinates done

--- core/src/com/codex/learning/states/StageSelectState.java
+++ core/src/com/codex/learning/states/StageSelectState.java
@@ -15,7 +15,7 @@ public class StageSelectState extends State{
 
     private TextureRegion stageSelect;
     private Vector3 touchpoint;
-    private Circle stages[] = new Circle[18];
+    private Circle stages[] = new Circle[17];
 
     public StageSelectState(Manager manager){
         super(manager);
@@ -23,25 +23,24 @@ public class StageSelectState extends State{
         stageSelect = new TextureRegion(new Texture(Constants.STAGE_SELECT_PATH));
 
         touchpoint = new Vector3();
-        stages[0] = new Circle(67, 135, 38);
-        stages[1] = new Circle(232, 135, 38);
-        stages[2] = new Circle(450, 135, 38);
-        stages[3] = new Circle(570, 222, 38);
-        stages[4] = new Circle(795, 227, 38);
-        stages[5] = new Circle(910, 227, 38);
-        stages[6] = new Circle(1030, 227, 38);
-        stages[7] = new Circle(1190, 160, 38);
-        stages[8] = new Circle(1410, 160, 38);
-        stages[9] = new Circle(1410, 330, 38);
-        // NOT DONE
-        stages[10] = new Circle(1366, 455, 38);
-        stages[11] = new Circle(1366, 690, 38);
-//        stages[12] = new Circle(728, 690, 38);
-//        stages[13] = new Circle(426, 690, 38);
-//        stages[14] = new Circle(90, 690, 38);
-//        stages[15] = new Circle(90, 690, 38);
-//        stages[16] = new Circle(90, 690, 38);
-//        stages[17] = new Circle(90, 690, 38);
+        stages[0] = new Circle(Constants.STAGE_1_1_X, Constants.STAGE_1_1_Y, Constants.STAGE_RADIUS);
+        stages[1] = new Circle(Constants.STAGE_1_2_X, Constants.STAGE_1_2_Y, Constants.STAGE_RADIUS);
+        stages[2] = new Circle(Constants.STAGE_1_3_X, Constants.STAGE_1_3_Y, Constants.STAGE_RADIUS);
+        stages[3] = new Circle(Constants.STAGE_1_4_X, Constants.STAGE_1_4_Y, Constants.STAGE_RADIUS);
+        stages[4] = new Circle(Constants.STAGE_1_5_X, Constants.STAGE_1_5_Y, Constants.STAGE_RADIUS);
+        stages[5] = new Circle(Constants.STAGE_1_6_X, Constants.STAGE_1_6_Y, Constants.STAGE_RADIUS);
+        stages[6] = new Circle(Constants.STAGE_1_7_X, Constants.STAGE_1_7_Y, Constants.STAGE_RADIUS);
+        stages[7] = new Circle(Constants.STAGE_1_8_X, Constants.STAGE_1_8_Y, Constants.STAGE_RADIUS);
+        stages[8] = new Circle(Constants.STAGE_1_9_X, Constants.STAGE_1_9_Y, Constants.STAGE_RADIUS);
+        stages[9] = new Circle(Constants.STAGE_1_10_X, Constants.STAGE_1_10_Y, Constants.STAGE_RADIUS);
+        stages[10] = new Circle(Constants.STAGE_1_11_X, Constants.STAGE_1_11_Y, Constants.STAGE_RADIUS);
+        stages[11] = new Circle(Constants.STAGE_1_12_X, Constants.STAGE_1_12_Y, Constants.STAGE_RADIUS);
+        stages[12] = new Circle(Constants.STAGE_1_13_X, Constants.STAGE_1_13_Y, Constants.STAGE_RADIUS);
+        stages[13] = new Circle(Constants.STAGE_1_14_X, Constants.STAGE_1_14_Y, Constants.STAGE_RADIUS);
+        stages[14] = new Circle(Constants.STAGE_1_15_X, Constants.STAGE_1_15_Y, Constants.STAGE_RADIUS);
+        stages[15] = new Circle(Constants.STAGE_1_16_X, Constants.STAGE_1_16_Y, Constants.STAGE_RADIUS);
+        stages[16] = new Circle(Constants.STAGE_1_17_X, Constants.STAGE_1_17_Y, Constants.STAGE_RADIUS);
+
     }
     @Override
     public void update(float delta) {
@@ -63,62 +62,11 @@ public class StageSelectState extends State{
     public void input(){
         if(Gdx.input.justTouched()){
             manager.getCamera().unproject(touchpoint.set(Gdx.input.getX(), Gdx.input.getY(), 0));
-//            for(int i = 0; i < stages.length; i++){
-            if(stages[0].contains(touchpoint.x, touchpoint.y)){
-                System.out.println("You clicked at stage " + 1 + "!!");
+            for(int i = 0; i < stages.length; i++){
+                if(stages[i].contains(touchpoint.x, touchpoint.y)){
+                    System.out.println("You clicked at stage " + (i + 1)  + "!!");
+                }
             }
-            if(stages[1].contains(touchpoint.x, touchpoint.y)){
-                System.out.println("You clicked at stage " + 2 + "!!");
-            }
-            if(stages[2].contains(touchpoint.x, touchpoint.y)){
-                System.out.println("You clicked at stage " + 3 + "!!");
-            }
-            if(stages[3].contains(touchpoint.x, touchpoint.y)){
-                System.out.println("You clicked at stage " + 4 + "!!");
-            }
-            if(stages[4].contains(touchpoint.x, touchpoint.y)){
-                System.out.println("You clicked at stage " + 5 + "!!");
-            }
-            if(stages[5].contains(touchpoint.x, touchpoint.y)){
-                System.out.println("You clicked at stage " + 6 + "!!");
-            }
-            if(stages[6].contains(touchpoint.x, touchpoint.y)){
-                System.out.println("You clicked at stage " + 7 + "!!");
-            }
-            if(stages[7].contains(touchpoint.x, touchpoint.y)){
-                System.out.println("You clicked at stage " + 8 + "!!");
-            }
-            if(stages[8].contains(touchpoint.x, touchpoint.y)){
-                System.out.println("You clicked at stage " + 9 + "!!");
-            }
-            if(stages[9].contains(touchpoint.x, touchpoint.y)){
-                System.out.println("You clicked at stage " + 10 + "!!");
-            }
-            if(stages[10].contains(touchpoint.x, touchpoint.y)){
-                System.out.println("You clicked at stage " + 11 + "!!");
-            }
-            if(stages[11].contains(touchpoint.x, touchpoint.y)){
-                System.out.println("You clicked at stage " + 12 + "!!");
-            }
-            if(stages[12].contains(touchpoint.x, touchpoint.y)){
-                System.out.println("You clicked at stage " + 13 + "!!");
-            }
-            if(stages[13].contains(touchpoint.x, touchpoint.y)){
-                System.out.println("You clicked at stage " + 14 + "!!");
-            }
-            if(stages[14].contains(touchpoint.x, touchpoint.y)){
-                System.out.println("You clicked at stage " + 15 + "!!");
-            }
-            if(stages[15].contains(touchpoint.x, touchpoint.y)){
-                System.out.println("You clicked at stage " + 16 + "!!");
-            }
-            if(stages[16].contains(touchpoint.x, touchpoint.y)){
-                System.out.println("You clicked at stage " + 17 + "!!");
-            }
-            if(stages[17].contains(touchpoint.x, touchpoint.y)){
-                System.out.println("You clicked at stage " + 18 + "!!");
-            }
-            //            }
         }
     }
 
--- core/src/com/codex/learning/utility/Constants.java
+++ core/src/com/codex/learning/utility/Constants.java
@@ -65,11 +65,41 @@ public class Constants {
 //    STAGE SELECT STATE
 
     //STAGE SELECT POINTS
-//    public static final int STAGE_1_1_X =
-//    public static final int STAGE_1_1_Y =
-//    public static final int STAGE_1_1_W =
-//    public static final int STAGE_1_1_H =
-
+    public static final int STAGE_RADIUS = 39;
+    public static final int STAGE_1_1_X = 67;
+    public static final int STAGE_1_1_Y = 135;
+    public static final int STAGE_1_2_X = 232;
+    public static final int STAGE_1_2_Y = 135;
+    public static final int STAGE_1_3_X = 450;
+    public static final int STAGE_1_3_Y = 135;
+    public static final int STAGE_1_4_X = 570;
+    public static final int STAGE_1_4_Y = 222;
+    public static final int STAGE_1_5_X = 795;
+    public static final int STAGE_1_5_Y = 227;
+    public static final int STAGE_1_6_X = 910;
+    public static final int STAGE_1_6_Y = 227;
+    public static final int STAGE_1_7_X = 1030;
+    public static final int STAGE_1_7_Y = 227;
+    public static final int STAGE_1_8_X = 1190;
+    public static final int STAGE_1_8_Y = 160;
+    public static final int STAGE_1_9_X = 1410;
+    public static final int STAGE_1_9_Y = 160;
+    public static final int STAGE_1_10_X = 1410;
+    public static final int STAGE_1_10_Y = 330;
+    public static final int STAGE_1_11_X = 1366;
+    public static final int STAGE_1_11_Y = 455;
+    public static final int STAGE_1_12_X = 1366;
+    public static final int STAGE_1_12_Y = 690;
+    public static final int STAGE_1_13_X = 1150;
+    public static final int STAGE_1_13_Y = 690;
+    public static final int STAGE_1_14_X = 970;
+    public static final int STAGE_1_14_Y = 690;
+    public static final int STAGE_1_15_X = 690;
+    public static final int STAGE_1_15_Y = 690;
+    public static final int STAGE_1_16_X = 415;
+    public static final int STAGE_1_16_Y = 690;
+    public static final int STAGE_1_17_X = 130;
+    public static final int STAGE_1_17_Y = 690;
 
 //    END OF STAGE SELECT STATE
 
