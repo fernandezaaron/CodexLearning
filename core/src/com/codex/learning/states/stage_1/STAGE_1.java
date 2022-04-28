977a4d6c5b72fbf5e034c5755fb75226db2d011e
Dracogray24 <85553906+Dracogray24@users.noreply.github.com>
2022-04-28 23:50:15 +0800

    stage1

--- core/src/com/codex/learning/states/stage_1/STAGE_1.java
+++ core/src/com/codex/learning/states/stage_1/STAGE_1.java
@@ -1,157 +1,123 @@
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+package com.mygdx.game;
 
-public class STAGE_1 extends JFrame implements KeyListener{
+import com.badlogic.gdx.*;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.ScreenUtils;
 
+public class STAGE_1 extends Game {
+    Stage stage_1;
+    SpriteBatch STAGE;
+    Texture Jedisaur, BACKGROUND;
+    float SPEED = 100;
+    float JEDISAUR_X = 200;
+    float JEDISAUR_Y = 270;
+    float JEDISAUR_W = 100;
+    float JEDISAUR_H = 100;
     int WALK_COUNTER;
-    JLabel JEDISAUR;
-    ImageIcon SPRITE_MODEL,BACKGROUND;
+    Screen stage1;
 
-    STAGE_1(){
-        // FRAME
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(1200,700);
-        this.setLayout(null);
-        this.addKeyListener(this);
-        this.setResizable(false);
+    public class stage1 extends InputAdapter implements Screen {
 
-        // BACKGROUND IMAGE
-        BACKGROUND = new ImageIcon("src\\com\\codex\\learning\\states\\stage_1\\house.png"); // FILE PATH
-        Image TEMP = BACKGROUND.getImage();
-        Image TEMP_BACKGROUND = TEMP.getScaledInstance(1200,660,Image.SCALE_SMOOTH);
-        BACKGROUND = new ImageIcon(TEMP_BACKGROUND);
-        JLabel mainBackground = new JLabel(BACKGROUND,JLabel.CENTER);
-        mainBackground.setIcon(BACKGROUND);
-        mainBackground.setBounds(0,-17,1200,700);
+        @Override
+        public void show() {
+            Jedisaur = new Texture("badlogic.jpg"); // Insert jedisaur sprite
+            BACKGROUND = new Texture("house.png"); // Main background
+            stage_1 = new Stage();
+            Gdx.input.setInputProcessor(stage_1);
+            STAGE = new SpriteBatch();
+        }
 
-
-        // JEDISAUR PLAYER
-        JEDISAUR = new JLabel();
-        JEDISAUR.setBounds(500, 300, 50, 50);
-        // JEDISAUR.setIcon(SPRITE_MODEL); // FOR JEDISAUR SPRITE
-        JEDISAUR.setBackground(Color.red);
-        JEDISAUR.setOpaque(true);
-        this.add(JEDISAUR);
-        this.setVisible(true);
-        this.add(mainBackground);
-    }
-
-    @Override
-    public void keyTyped(KeyEvent e) {
-        //WASD KEYS
-        if(e.getKeyChar() == 'a' ||e.getKeyChar() == 'A' ||e.getKeyChar() == 'w' ||e.getKeyChar() == 'W' ||
-                e.getKeyChar() == 's' ||e.getKeyChar() == 'S' ||e.getKeyChar() == 'd' ||e.getKeyChar() == 'D') {
-            switch (e.getKeyChar()) {
-                case 'a':
-                case 'A':
-                    JEDISAUR.setLocation(JEDISAUR.getX() - 10, JEDISAUR.getY());
-                    if (WALK_COUNTER == 1) {
-                        //JEDISAUR.setBackground(Color.blue); EXAMPLE FOR WALK ANIMATIONS
-                    } else if (WALK_COUNTER == 2) {
-                        //label.setBackground(Color.green);   EXAMPLE FOR WALK ANIMATIONS
-                    }
-
-                    break;
-                case 'w':
-                case 'W':
-                    JEDISAUR.setLocation(JEDISAUR.getX(), JEDISAUR.getY() - 10);
-                    if (WALK_COUNTER == 1) {
-                        //JEDISAUR.setBackground(Color.black);  EXAMPLE FOR WALK ANIMATIONS
-                    } else if (WALK_COUNTER == 2) {
-                        //JEDISAUR.setBackground(Color.yellow); EXAMPLE FOR WALK ANIMATIONS
-                    }
-                    break;
-                case 's':
-                case 'S':
-                    JEDISAUR.setLocation(JEDISAUR.getX(), JEDISAUR.getY() + 10);
-                    if (WALK_COUNTER == 1) {
-                        // JEDISAUR.setBackground(Color.white);  EXAMPLE FOR WALK ANIMATIONS
-                    } else if (WALK_COUNTER == 2) {
-                        //JEDISAUR.setBackground(Color.orange); EXAMPLE FOR WALK ANIMATIONS
-                    }
-                    break;
-                case 'd':
-                case 'D':
-                    JEDISAUR.setLocation(JEDISAUR.getX() + 10, JEDISAUR.getY());
-                    if (WALK_COUNTER == 1) {
-                        //  JEDISAUR.setBackground(Color.pink);  EXAMPLE FOR WALK ANIMATIONS
-                    } else if (WALK_COUNTER == 2) {
-                        //  JEDISAUR.setBackground(Color.gray);  EXAMPLE FOR WALK ANIMATIONS
-                    }
-                    break;
-            }
-            WALK_COUNTER++;
-            if (WALK_COUNTER > 2) {
+        @Override
+        public void render(float delta) {
+            Gdx.gl.glClearColor(1, 1, 1, 0);
+            ScreenUtils.clear(1, 1, 1, 0);
+            STAGE.begin();
+            stage_1.draw();
+            STAGE.draw(BACKGROUND,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+            STAGE.draw(Jedisaur, JEDISAUR_X, JEDISAUR_Y, JEDISAUR_W,JEDISAUR_H);
+            // Jedisaur walking animation counter
+            WALK_COUNTER ++;
+            if (WALK_COUNTER > 30) {
                 if (WALK_COUNTER == 1) {
                     WALK_COUNTER = 2;
                 } else if (WALK_COUNTER == 2) {
                     WALK_COUNTER = 1;
+                } else {
+                    WALK_COUNTER = 0;
                 }
-                WALK_COUNTER = 0;
 
             }
-        }else{
-            WALK_COUNTER = 1;
 
-        }
-    }
+            if (Gdx.input.isKeyPressed(Input.Keys.W)) {
 
-    @Override
-    public void keyPressed(KeyEvent e) {
-        // ARROW KEYS
-        if(e.getKeyCode() == 37 ||e.getKeyCode() == 38 ||e.getKeyCode() == 39 ||e.getKeyCode() == 40 ) {
-            switch (e.getKeyCode()) {
-                case 37:
-                    JEDISAUR.setLocation(JEDISAUR.getX() - 10, JEDISAUR.getY());
-                    if (WALK_COUNTER == 1) {
-                        //  JEDISAUR.setBackground(Color.pink); EXAMPLE FOR WALK ANIMATIONS
-                    } else if (WALK_COUNTER == 2) {
-                        // JEDISAUR.setBackground(Color.gray);  EXAMPLE FOR WALK ANIMATIONS
-                    }
-                    break;
-                case 38:
-                    JEDISAUR.setLocation(JEDISAUR.getX(), JEDISAUR.getY() - 10);
-                    if (WALK_COUNTER == 1) {
-                        //  JEDISAUR.setBackground(Color.pink);  EXAMPLE FOR WALK ANIMATIONS
-                    } else if (WALK_COUNTER == 2) {
-                        //  JEDISAUR.setBackground(Color.gray);  EXAMPLE FOR WALK ANIMATIONS
-                    }
-                    break;
-                case 39:
-                    JEDISAUR.setLocation(JEDISAUR.getX() + 10, JEDISAUR.getY());
-                    if (WALK_COUNTER == 1) {
-                        //   JEDISAUR.setBackground(Color.pink);  EXAMPLE FOR WALK ANIMATIONS
-                    } else if (WALK_COUNTER == 2) {
-                        //   JEDISAUR.setBackground(Color.gray);  EXAMPLE FOR WALK ANIMATIONS
-                    }
-                    break;
-                case 40:
-                    JEDISAUR.setLocation(JEDISAUR.getX(), JEDISAUR.getY() + 10);
-                    if (WALK_COUNTER == 1) {
-                        //   JEDISAUR.setBackground(Color.pink);  EXAMPLE FOR WALK ANIMATIONS
-                    } else if (WALK_COUNTER == 2) {
-                        //   JEDISAUR.setBackground(Color.gray);  EXAMPLE FOR WALK ANIMATIONS
-                    }
-                    break;
-            }
-            WALK_COUNTER++;
-            if (WALK_COUNTER > 2) {
+                System.out.print("W");
+                JEDISAUR_Y += Gdx.graphics.getDeltaTime() * SPEED;
                 if (WALK_COUNTER == 1) {
-                    WALK_COUNTER = 2;
+                    JEDISAUR_H = 50;		// Example for walking animation
                 } else if (WALK_COUNTER == 2) {
-                    WALK_COUNTER = 1;
+                    JEDISAUR_H = 100;		// Example for walking animation
                 }
-                WALK_COUNTER = 0;
+            } else if (Gdx.input.isKeyPressed(Input.Keys.S)) {
+                System.out.print("S");
+                JEDISAUR_Y -= Gdx.graphics.getDeltaTime() * SPEED;
+                if (WALK_COUNTER == 1) {
+                    JEDISAUR_H = 30;		// Example for walking animation
+                } else if (WALK_COUNTER == 2) {
+                    JEDISAUR_H = 90;		// Example for walking animation
+                }
+            } else if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+                System.out.print("A");
+                JEDISAUR_X -= Gdx.graphics.getDeltaTime() * SPEED;
+                if (WALK_COUNTER == 1) {
+                    JEDISAUR_W = 50;		// Example for walking animation
+                } else if (WALK_COUNTER == 2) {
+                    JEDISAUR_W = 100;		// Example for walking animation
+                }
+            } else if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+                System.out.print("D");
+                JEDISAUR_X += Gdx.graphics.getDeltaTime() * SPEED;
+                if (WALK_COUNTER == 1) {
+                    JEDISAUR_W = 30;		// Example for walking animation
+                } else if (WALK_COUNTER == 2) {
+                    JEDISAUR_W = 90;		// Example for walking animation
+                }
             }
-        }else{
-            WALK_COUNTER = 1;
+            STAGE.end();
+        }
+
+        @Override
+        public void resize(int width, int height) {
+
+        }
+
+        @Override
+        public void pause() {
+
+        }
+
+        @Override
+        public void resume() {
+
+        }
+
+        @Override
+        public void hide() {
+
+        }
+
+        @Override
+        public void dispose() {
+
         }
     }
     @Override
-    public void keyReleased(KeyEvent e) {
+    public void create () {
+        Gdx.graphics.setWindowedMode(1600,920);
+        stage1 = new stage1();
+        setScreen(stage1);
     }
-    public static void main(String[] args) {
-        new STAGE_1();
-    }
+
+
 }
