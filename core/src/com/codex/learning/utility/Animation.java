d505ffa27984b17b6074add1b7b9a6d1ea702124
Jedisaur <fernandezaaron007@gmail.com>
2022-05-04 20:31:56 +0800

    animation

--- core/src/com/codex/learning/utility/Animation.java
+++ core/src/com/codex/learning/utility/Animation.java
@@ -4,7 +4,7 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.Array;
 
 public class Animation {
-
+    //animation
     private Array<TextureRegion> frames;
     private float maxFrameTime;
     private float currFrameTime;
