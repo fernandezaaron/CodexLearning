a0148582d206f421b20be1c72ae006ee231a97f3
Paul Co <79306634+Paul-26-Co@users.noreply.github.com>
2022-05-15 14:49:45 +0800

    WIP

--- core/src/com/codex/learning/entity/Blocks.java
+++ core/src/com/codex/learning/entity/Blocks.java
@@ -1,11 +1,18 @@
 package com.codex.learning.entity;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.codex.learning.utility.Constants;
 import com.codex.learning.utility.Manager;
 
 public class Blocks extends Entity{
-
+    private TextureRegion button[] = new TextureRegion[20];
     public Blocks(Manager manager){
         super(manager);
     }
@@ -14,15 +21,49 @@ public class Blocks extends Entity{
     public void create(Vector2 position, Vector2 size, float density) {
         this.position = position;
         this.size = size;
+
+        this.size.x /= Constants.PPM;
+        this.size.y /= Constants.PPM;
+
+        BodyDef def = new BodyDef();
+        def.type = BodyDef.BodyType.DynamicBody;
+        def.position.set(this.position);
+        def.fixedRotation = true;
+
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(this.size.x, this.size.y);
+
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.density = density;
+        fixtureDef.shape = shape;
+        fixtureDef.friction = 0.75f;
+
+        body = manager.getWorld().createBody(def);
+        body.createFixture(fixtureDef);
+        shape.dispose();
+
     }
 
     @Override
     public void update(float delta) {
-
+        cameraUpdate();
     }
 
     @Override
     public void render(SpriteBatch sprite) {
-
+        sprite.enableBlending();
+        sprite.setProjectionMatrix(manager.getCamera().combined);
+        sprite.begin();
+        sprite.draw(manager.getBadLogic(), 100, 0, this.size.x, this.size.y);
+        sprite.end();
     }
+
+    private void cameraUpdate(){
+        Vector3 position = manager.getCamera().position;
+        position.x = this.position.x * Constants.PPM;
+        position.y = this.position.y * Constants.PPM;
+        manager.getCamera().position.set(position);
+        manager.getCamera().update();
+    }
+
 }
--- core/src/com/codex/learning/states/PlayState.java
+++ core/src/com/codex/learning/states/PlayState.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
+import com.codex.learning.entity.Blocks;
 import com.codex.learning.entity.Jedisaur;
 import com.codex.learning.utility.Constants;
 import com.codex.learning.utility.Contact;
@@ -15,13 +16,14 @@ import java.awt.*;
 public class PlayState extends State{
 
     private Jedisaur character;
-    private Texture badLogic;
-
+    private Blocks blocks;
     public PlayState(Manager manager) {
         super(manager);
         character = new Jedisaur(manager);
         character.create(new Vector2(0,0),new Vector2(42, 60),1.6f);
-        badLogic = new Texture("badLogic.jpg");
+
+        blocks = new Blocks(manager);
+        blocks.create(new Vector2(200,0),new Vector2(50, 50),1.6f );
 
     }
 
@@ -38,9 +40,9 @@ public class PlayState extends State{
         sprite.begin();
         sprite.setProjectionMatrix(manager.getCamera().combined);
         sprite.draw(manager.getStage1(), manager.getCamera().position.x - Constants.SCREEN_WIDTH/2f,manager.getCamera().position.y - Constants.SCREEN_HEIGHT/2f,Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT);
-        sprite.draw(badLogic, 200, 0, 100, 100);
         sprite.end();
         character.render(sprite);
+        blocks.render(sprite);
 
     }
 
