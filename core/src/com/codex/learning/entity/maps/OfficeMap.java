999ee13f70249e54221e2a7c19efbbc59292d19e
Jedisaur <fernandezaaron007@gmail.com>
2022-08-31 18:37:38 +0800

    officemap

---
+++ core/src/com/codex/learning/entity/maps/OfficeMap.java
@@ -0,0 +1,33 @@
+package com.codex.learning.entity.maps;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.codex.learning.entity.Collisions;
+import com.codex.learning.states.State;
+import com.codex.learning.utility.Manager;
+
+public class OfficeMap extends State {
+
+    private Collisions leftBorder, upBorder, downBorder, playroomBorder;
+    public OfficeMap(Manager manager){
+        super(manager);
+
+
+
+
+
+    }
+    @Override
+    public void update(float delta) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sprite) {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
