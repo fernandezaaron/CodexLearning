a832ef39d403b18b438f22f6ee85621cc62f9c77
RjEsg <rjme101400@gmail.com>
2022-09-09 00:19:29 +0800

    unfinished

--- core/src/com/codex/learning/states/minigames/MysteryCode.java
+++ core/src/com/codex/learning/states/minigames/MysteryCode.java
@@ -30,7 +30,7 @@ public class MysteryCode extends State {
     float blockSize;
 
     private ArrayList<ArrayList<String>> minigameContainer;
-    private int minigameContainerLimit;
+    private ArrayList<ArrayList<Integer>> banishPerRow;
     private Random randomizer;
     private ArrayList<Integer> banishCells;
     private ArrayList<String> answerPoolContainer;
@@ -57,9 +57,35 @@ public class MysteryCode extends State {
 
         getAMinigame(manager.getStageSelector().map(), manager.getExpertSystem().getExpertiseLevel());
 
-        for(int i = 0; i <= 10; i++) {
-            banishCells.add(randomizer.nextInt(minigameContainerLimit - 1) + 1);
+        // GENERATES THE NUMBER OF THE BLOCKS TO BE REMOVED
+        for(int i = 0; i < banishPerRow.size(); i++) {
+            int banishNumberIterator = randomizer.nextInt(2) + 1;
+            int numberRepeat = 0;
+            System.out.println("how many? " + banishNumberIterator);
+            for(int j = 0; j < banishPerRow.get(i).size(); j++) {
+//                System.out.println((banishPerRow.get(i).size()) + "    " + banishPerRow.get(i).get(0));
+                int banishNumber = randomizer.nextInt(banishPerRow.get(i).size() - 1) + banishPerRow.get(i).get(0);
+                System.out.println(banishNumber);
+                if(banishNumberIterator == 0 || numberRepeat == 5) {
+//                    System.out.println("berak");
+                    break;
+                }
+                else if(!banishCells.contains(banishNumber)) {
+                    banishCells.add(banishNumber);
+                    banishNumberIterator--;
+//                    System.out.println("baka sa iterator " + banishNumberIterator +
+//                            " eh sa last element " + (banishPerRow.get(i).size() - 1) +
+//                            " first? " + banishPerRow.get(i).get(0) +
+//                            " eto ibabanish " + banishNumber);
+                }
+                else {
+                    System.out.println("number repeated");
+                    numberRepeat++;
+                }
+            }
         }
+//        System.out.println("nagbreak?");
+
 
         // START MINIGAME CREATION
         int yStartingPoint = 11, currentCell = 0;
