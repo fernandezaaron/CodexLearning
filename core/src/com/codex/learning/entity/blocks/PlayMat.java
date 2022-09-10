3b80bdb3cb5a3b3956ced5e7c8ba8d6217f3b1cd
Jedisaur <fernandezaaron007@gmail.com>
2022-09-11 00:33:58 +0800

    Playmat

---
+++ core/src/com/codex/learning/entity/blocks/PlayMat.java
@@ -0,0 +1,58 @@
+package com.codex.learning.entity.blocks;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.codex.learning.entity.Entity;
+import com.codex.learning.states.minigames.CodeRiddle;
+import com.codex.learning.utility.Constants;
+import com.codex.learning.utility.Manager;
+
+public class PlayMat extends Entity {
+
+
+
+    public PlayMat(Manager manager){
+        super(manager);
+    }
+
+
+    @Override
+    public void create(Vector2 position, Vector2 size, float density) {
+        this.position = position;
+        this.size = size;
+
+        BodyDef def = new BodyDef();
+        def.type = BodyDef.BodyType.StaticBody;
+        def.position.set(this.position);
+        def.fixedRotation = true;
+
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(this.size.x , this.size.y);
+
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.density = density;
+        fixtureDef.shape = shape;
+        fixtureDef.isSensor = true;
+        fixtureDef.friction = 5;
+
+        body = manager.getWorld().createBody(def);
+        body.createFixture(fixtureDef).setUserData(this);
+        body.setLinearVelocity(0, 0);
+        shape.dispose();
+
+    }
+
+    @Override
+    public void update(float delta) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sprite) {
+
+    }
+}
