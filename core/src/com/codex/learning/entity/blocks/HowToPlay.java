a0705b194f2a198af0651daaab7302f27348b1ec
Paul Co <paulteves353@gmail.com>
2022-09-12 01:15:54 +0800

    WIP howtoplay

---
+++ core/src/com/codex/learning/entity/blocks/HowToPlay.java
@@ -0,0 +1,27 @@
+package com.codex.learning.entity.blocks;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.codex.learning.entity.Entity;
+import com.codex.learning.utility.Manager;
+
+public class HowToPlay extends Entity {
+    public HowToPlay(Manager manager) {
+        super(manager);
+    }
+
+    @Override
+    public void create(Vector2 position, Vector2 size, float density) {
+
+    }
+
+    @Override
+    public void update(float delta) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sprite) {
+
+    }
+}
