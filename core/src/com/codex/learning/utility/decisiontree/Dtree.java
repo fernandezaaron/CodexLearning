b28f280d2e7fee2b7a1e2b9821a7ddb5ff365648
Paul Co <paulteves353@gmail.com>
2022-09-20 23:43:01 +0800

    WIP ml

--- core/src/com/codex/learning/utility/decisiontree/Dtree.java
+++ core/src/com/codex/learning/utility/decisiontree/Dtree.java
@@ -1,19 +1,19 @@
 package com.codex.learning.utility.decisiontree;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.ProcessBuilder;
 
 public class Dtree {
-    public static String removeBracket(String string){
+    public String removeBracket(String string){
         StringBuilder stringBuilder = new StringBuilder(string);
 
         stringBuilder.deleteCharAt(string.length() - 1);
         stringBuilder.deleteCharAt(string.length() - 2);
         stringBuilder.deleteCharAt(1);
         stringBuilder.deleteCharAt(0);
-
 
         return stringBuilder.toString();
     }
@@ -22,7 +22,7 @@ public class Dtree {
         String s = null;
         try {
             ProcessBuilder processBuilder = new ProcessBuilder("python",
-                    System.getProperty("user.dir") + "\\model\\minigameScript.py",
+                    System.getProperty("user.dir") + "\\assets\\model\\minigameScript.py",
                     movementDetected, timeConsumption, numberOfAttempt, numberOfBlockInteraction);
 
             Process process = processBuilder.start();
@@ -35,31 +35,23 @@ public class Dtree {
         }catch(IOException e){
             e.printStackTrace();
         }
-        return "www";
+        return "";
     }
 
-    public static String codeRiddleML(String timeConsumption, String numberOfError){
+    public String codeRiddleML(String timeConsumption, String numberOfError){
         try {
             ProcessBuilder processBuilder = new ProcessBuilder("python",
-                    System.getProperty("user.dir") + "\\model\\codeRiddleScript.py",
+                    System.getProperty("user.dir") + "\\assets\\model\\codeRiddleScript.py",
                     timeConsumption, numberOfError);
             Process process = processBuilder.start();
             BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
             String s = null;
-
             while ((s = reader.readLine()) != null) {
-//                currentBehavior = removeBracket(s);
-//                System.out.println(removeBracket(s));
-//                System.out.println("ASDA - " + currentBehavior);
                 return removeBracket(s);
             }
         }catch(IOException e){
             e.printStackTrace();
         }
-        return "ss";
-    }
-
-    public static void main(String[] args) {
-        System.out.println(codeRiddleML("0", "5"));
+        return "";
     }
 }
