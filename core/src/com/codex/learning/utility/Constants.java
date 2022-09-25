85c68ad7d3b3dd94626b5647514b794bc3be4e6a
Jedisaur <fernandezaaron007@gmail.com>
2022-09-26 02:02:08 +0800

    up

--- core/src/com/codex/learning/utility/Constants.java
+++ core/src/com/codex/learning/utility/Constants.java
@@ -40,13 +40,13 @@ public class Constants {
     public static final String JSON_DIALOG_BOX_SKIN_PATH = "text/DialogBox.json";
 
     // SAVE FILE ASSET PATH
-    public static final String SAVE_FILE_PATH = "assets/text/SaveFile.txt";
-    public static final String DATA_GATHERED_FILE_PATH = "assets/text/data.txt";
-    public static final String CODE_RIDDLE_DATA_FILE_PATH = "assets/text/codeRiddleData.txt";
-    public static final String MINIGAME_DATA_FILE_PATH = "assets/text/minigameData.txt";
+    public static final String SAVE_FILE_PATH = "text/SaveFile.txt";
+    public static final String DATA_GATHERED_FILE_PATH = "text/data.txt";
+    public static final String CODE_RIDDLE_DATA_FILE_PATH = "text/codeRiddleData.txt";
+    public static final String MINIGAME_DATA_FILE_PATH = "text/minigameData.txt";
 
     // EXCEL FILE ASSET PATH
-    public static final String EXCEL_FILE_PATH = "assets/excel/Minigames.xlsx";
+    public static final String EXCEL_FILE_PATH = "excel/Minigames.xlsx";
 
     // ATLAS ASSET PATH
     public static final String ATLAS_UTILITY_PATH = "text/DialogBox.atlas";
