21847bc0d2dcee01d2e7e7f1c6a4ee40e1595000
Jedisaur <fernandezaaron007@gmail.com>
2022-09-25 19:30:59 +0800

    Revert "merge with jy"
    This reverts commit e50a2dfaf6fd4ef31689fdb98587872af95a1a8d.

--- core/src/com/codex/learning/states/minigames/MysteryCode.java
+++ core/src/com/codex/learning/states/minigames/MysteryCode.java
@@ -131,7 +131,11 @@ public class MysteryCode extends State {
                 }
                 currentStringLength = (float) String.valueOf(answerPoolContainer.get(currentAnsCell)).length();
                 totalLineLength += currentStringLength + (AnsPoolX - 11);
+<<<<<<< HEAD
                 answerBlocks[currentAnsCell] = new Blocks(manager,  answerPoolContainer.get(currentAnsCell), answerPoolContainer.get(currentAnsCell), false);
+=======
+                answerBlocks[currentAnsCell] = new Blocks(manager, answerPoolContainer.get(currentAnsCell), answerPoolContainer.get(currentAnsCell), false);
+>>>>>>> jy_test
                 if (answerPoolContainer.get(currentAnsCell) != null) {
                     if (currentStringLength <= 3) {
                         answerBlocks[currentAnsCell].create(new Vector2(AnsPoolX, AnsPoolY), new Vector2((currentStringLength * 0.5f), Constants.BLOCKS_HEIGHT), 0);
