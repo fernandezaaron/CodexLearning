bc481ffb6a18fe3f3ff68aab426c6fc2dc8b1641
Jedisaur <fernandezaaron007@gmail.com>
2022-10-01 01:12:31 +0800

    cr

--- core/src/com/codex/learning/states/minigames/CodeRiddle.java
+++ core/src/com/codex/learning/states/minigames/CodeRiddle.java
@@ -277,7 +277,6 @@ public class CodeRiddle extends State {
                table.pack();
            }
             manager.getStage().addActor(table);
-           manager.getStage().setDebugAll(true);
         }
     }
 
