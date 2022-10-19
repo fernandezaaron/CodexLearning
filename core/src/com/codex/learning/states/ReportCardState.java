91f7aff7d8e4184cc00a4be723f11873a8becf5f
Jedisaur <fernandezaaron007@gmail.com>
2022-10-20 01:44:19 +0800

    reportcardstate

---
+++ core/src/com/codex/learning/states/ReportCardState.java
@@ -0,0 +1,36 @@
+package com.codex.learning.states;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.codex.learning.utility.Manager;
+import jdk.javadoc.internal.doclets.formats.html.markup.Table;
+
+public class ReportCardState extends State{
+
+    private Table backgroundTable, textButtonContainer, reportCardTable, containerTable;
+    private TextButton[] textButtons;
+    private Label
+
+    public ReportCardState(Manager manager){
+        super(manager);
+
+
+
+    }
+
+    @Override
+    public void update(float delta) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sprite) {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
